c4c8ea2b2a5554c892d88c655f68729085ef31d4
Tess <tess.mueske@gmail.com>
2024-01-26 13:25:41 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array){
+const winningObject = array.find (object => object.result === "W")
+  if (winningObject) {
+    return winningObject.year;
+  } else {
+    return undefined;
+  }
+}
